f45f9e771b29388644598184706374ca07cffdfe
Tablerase <tabler3se@gmail.com>
2023-05-18 17:40:07 +0200

    js for title support + add reddit API

--- post.py
+++ post.py
@@ -1,25 +1,32 @@
 import tweepy
 import configparser
+import praw
 
 #read configs
 config  = configparser.ConfigParser()
 config.read('config.ini')
 
+#twitter configs
 bearer = config['twitter']['bearer']
 twitter_c_key = config['twitter']['api_key']
 twitter_c_key_secret = config['twitter']['api_key_secret']
 twitter_a_token = config['twitter']['access_token']
 twitter_a_token_secret = config['twitter']['access_token_secret']
-twitter = tweepy.Client(
+#reddit configs
+reddit_id = config['reddit']['client_id']
+reddit_secret = config['reddit']['client_secret']
+reddit_agent = config['reddit']['user_agent']
+reddit_name = config['reddit']['username']
+reddit_passwrd = config['reddit']['password']
+
+# https://docs.tweepy.org/en/stable/client.html#tweepy.Client.create_tweet
+def tweet_create(msg):
+    twitter = tweepy.Client(
         consumer_key=twitter_c_key,
         consumer_secret=twitter_c_key_secret,
         access_token=twitter_a_token,
         access_token_secret=twitter_a_token_secret
     )
-
-# https://docs.tweepy.org/en/stable/client.html#tweepy.Client.create_tweet
-def tweet_create(msg):
-    
     response = twitter.create_tweet(
         text=f"{msg}"
     )
@@ -36,3 +43,21 @@ def tweet_poll(msg, poll_option_list, poll_duration):
     )
     return response.data['id'], response.data['text'], response.data['poll_options']
 '''
+
+# https://praw.readthedocs.io/en/stable/code_overview/other/submissionmoderation.html
+def reddit_create(msg, title_reddit):
+    reddit = praw.Reddit(
+        client_id = reddit_id,
+        client_secret = reddit_secret,
+        user_agent = reddit_agent,
+        username = reddit_name,
+        password = reddit_passwrd)
+
+    # get subreddit for publishing
+    sub = "Tablerase"
+    subreddit = reddit.subreddit(sub)
+    # submit content
+    submission = subreddit.submit(title= title_reddit, selftext= msg)
+
+    resplist = {"id":submission.id, "text": f"{title_reddit} {msg}", "link": f"https://reddit.com{submission.permalink}"}
+    return resplist
--- test.py
+++ test.py
@@ -5,15 +5,27 @@ import praw
 config  = configparser.ConfigParser()
 config.read('config.ini')
 
-client_id = config['reddit']['client_id']
-client_secret = config['reddit']['client_secret']
+reddit_id = config['reddit']['client_id']
+reddit_secret = config['reddit']['client_secret']
+reddit_agent = config['reddit']['user_agent']
+reddit_name = config['reddit']['username']
+reddit_passwrd = config['reddit']['password']
+
+print(reddit_id, reddit_secret, reddit_agent, reddit_name, reddit_passwrd)
 
 reddit = praw.Reddit(
-    client_id="Q3dfveBJYo_4FTwIdlKKmQ",
-    client_secret="jUAAzmmqe9uVxOBqlJTUoZRxsAvAow",
-    user_agent="_Tablerase",
-)
+    client_id = reddit_id,
+    client_secret = reddit_secret,
+    user_agent = reddit_agent,
+    username = reddit_name,
+    password = reddit_passwrd)
+
 
 # get subreddit for publishing
-subreddit = reddit.subreddit("Tablerase")
+sub = "Tablerase"
+subreddit = reddit.subreddit(sub)
+submission = subreddit.submit(title="Test Submission", selftext="This is a test")
+
 print(subreddit.display_name)
+print(f"https://reddit.com{submission.permalink}")
+print(submission.id)
